7edcc6400e21be0223335c4744f6f1489f60c520
Khoi Nguyen <khoinguyen311@gmail.com>
2026-03-16 01:14:30 -0700

    Add distance markers along ground to show spatial movement. Minor ticks every 200px, labeled majors every 1000px.

--- src/game/scenes/MainGame.ts
+++ src/game/scenes/MainGame.ts
@@ -18,6 +18,7 @@ export class MainGame extends Phaser.Scene {
     this.physics.world.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT)
 
     this.createGround()
+    this.createDistanceMarkers()
     this.player = new Player(this, 100, GROUND_Y - 48)
     this.physics.add.collider(
       this.player as unknown as Phaser.Physics.Arcade.Sprite,
@@ -34,6 +35,28 @@ export class MainGame extends Phaser.Scene {
     this.ground = this.physics.add.staticGroup()
     for (let x = 0; x < WORLD_WIDTH; x += 32) {
       this.ground.create(x + 16, GROUND_Y, 'ground')
+    }
+  }
+
+  private createDistanceMarkers() {
+    const gfx = this.add.graphics()
+
+    for (let x = 200; x < WORLD_WIDTH; x += 200) {
+      const isMajor = x % 1000 === 0
+
+      // Tick mark above the ground
+      gfx.fillStyle(isMajor ? 0xffffff : 0x888899, isMajor ? 1 : 0.6)
+      gfx.fillRect(x - 1, GROUND_Y - 48, 2, isMajor ? 40 : 20)
+
+      // Label every 1000px
+      if (isMajor) {
+        this.add
+          .text(x, GROUND_Y - 60, `${x}`, {
+            fontSize: '11px',
+            color: '#aaaacc',
+          })
+          .setOrigin(0.5, 1)
+      }
     }
   }
 }
